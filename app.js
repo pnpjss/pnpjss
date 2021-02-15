3512f5388fcc50c53a917fe2112c6cb2660ea729
pnpjss <pnpjss@github.com>
2021-02-15 13:32:28 +0100

    l1

--- app.js
+++ app.js
@@ -1,14 +1,5 @@
-let lucas = document.querySelector(".lucas");
-let nordeborg = document.querySelector(".nordeborg");
 let webDev = document.querySelector(".web-developer");
 let inDev = document.querySelector(".in-developement");
-window.onload = () => {
-	lucas.style.margin = `0`;
-	nordeborg.style.margin = `0`;
-	setTimeout(() => {
-		inDev.style.opacity = `100`;
-	}, 340);
-};
 
 let btnBgSwe = document.querySelector("#bgleft");
 let btnTextSwe = document.querySelector("#swe");
@@ -18,7 +9,6 @@ let btnTextEng = document.querySelector("#texteng");
 const btnSwe = document.querySelector(".btn-swe");
 
 btnSwe.addEventListener("click", () => {
-	console.log("fart");
 	btnBgSwe.style.fill = "#b64100";
 	btnTextSwe.style.fill = "white";
 
@@ -29,7 +19,6 @@ btnSwe.addEventListener("click", () => {
 const btnEng = document.querySelector(".btn-eng");
 
 btnEng.addEventListener("click", () => {
-	console.log("fart");
 	btnBgSwe.style.fill = "white";
 	btnTextSwe.style.fill = "#b64100";
 
@@ -46,15 +35,13 @@ let hamState = 0;
 btnHam.addEventListener("click", () => {
 	if (hamState === 1) {
 		hamState--;
-		webDev.style.zIndex = `1`;
-		inDev.style.zIndex = `1`;
+
 		hamup.style.transform = `translateX(0px) rotateZ(0deg)`;
 		hamdown.style.transform = `translateX(0px) translateY(0px) rotateZ(0deg)`;
 		hamMenu.style.zIndex = `-1`;
 	} else if (hamState === 0) {
 		hamState++;
-		webDev.style.zIndex = `0`;
-		inDev.style.zIndex = `0`;
+
 		hamup.style.transform = `translateX(10px) rotateZ(45deg)`;
 		hamdown.style.transform = `translateX(-15px) translateY(20px) rotateZ(-45deg)`;
 		hamMenu.style.zIndex = `3`;
